599b9750599c3512da5cbe4dc8c83e2742276e60
rishibathina <39679104+rishibathina@users.noreply.github.com>
2024-05-18 18:28:26 -0700

    made a lot of changes, enumeration is done i think

--- taskverse/mathqa/geometry_task.py
+++ taskverse/mathqa/geometry_task.py
@@ -9,6 +9,9 @@ from utils import *
 # PERI_SIDE_ONE_RANGE, PERI_SIDE_TWO_RANGE, PERI_SIDE_THREE_RANGE, 
 #     make_single_prod, make_pair_prod, make_triplet_prod
 
+class Template(Enum):
+    PERIMETER_TEMPLATES = 'MathVerse/math_anotations/perimeter_templates.json'
+
 class GeoPlanGenerator(TaskGenerator):
     def __init__(self, metadata={}, seed=42):
         super().__init__(metadata, seed=seed)
@@ -19,6 +22,7 @@ class GeoPlanGenerator(TaskGenerator):
     def _generate_task(self, task_plan) -> Tuple[str, str, List[str], Dict]:
         "(Abstract method) generate task"
         # TODO: COME BACK IN FILL THIS IN AS NOT A ABSTRACT METHOD
+        
     
     def generate(self, task_plan, return_data=True, seed=None):
         if seed is not None:
@@ -63,17 +67,57 @@ class PerimeterGenerator(GeoPlanGenerator):
         self.side_one_range = PERI_SIDE_ONE_RANGE
         self.side_two_range = PERI_SIDE_TWO_RANGE
         self.side_three_range = PERI_SIDE_THREE_RANGE
+        self.int_to_peri_list = int_to_peri_list
+        
+    def handle_templates(template_path):
+        # iterate through json file and seperate into different number of params
+        templates_by_num_params = {}
+
+        with open(file_path, 'r') as file:
+            templates_data = json.load(file)
+
+        # Iterate over each template
+        for template_name, template_info in templates_data.items():
+            num_params = template_info["num_params"]
+            template_text = template_info["text"]
+
+            # Add the template text to the corresponding list based on the number of parameters
+            if num_params not in templates_by_num_params:
+                templates_by_num_params[num_params] = []
+            templates_by_num_params[num_params].append(template_text)
+
+        return templates_by_num_params
 
     def enumerate_task_plans(self, task_store: TaskStore):
-        single = make_single_prod(side_one_range)
-        pairs = make_pair_prod(side_one_range, side_three_range)
-        triplets = make_triplet_prod(side_one_range, side_two_range, side_three_range)
+        single = make_single_prod(self.side_one_range)
+        pairs = make_pair_prod(self.side_one_range, self.side_three_range)
+        triplets = make_triplet_prod(self.side_one_range, self.side_two_range, self.side_three_range)
         
+        template_path = Template.PERIMETER_TEMPLATES
+        template_breakdown = handle_templates(template_path)
         
-        return super().enumerate_task_plans(task_store)
-    
+        for param_count, templates in template_breakdown.items():
+            peri_list = locals()[self.int_to_peri_list[param_count]]
+            
+            for template in tqdm(templates, desc=f"Enumerating templates with {param_count} params"):
+                for group in peri_list:
+                    params = [param if param is not None else None for param in group]
+                    while len(params) < 3:
+                        params.append(None)
+
+                    task_plan = {
+                        'question_template': template,
+                        'side_one': params[0],
+                        'side_two': params[1],
+                        'side_three': params[2]
+                    }
+                    
+                    task_store.add(task_plan)
+                
+            
     def _generate_task(self, task_plan) -> Tuple[str | List[str] | Dict]:
-        return super()._generate_task(task_plan)
+        
+        pass
     
 # class AreaGenerator(GeoPlanGenerator):
 #     schema = {
--- taskverse/mathqa/utils.py
+++ taskverse/mathqa/utils.py
@@ -7,10 +7,16 @@ PERI_SIDE_ONE_RANGE = (1.0, 20.0)
 PERI_SIDE_TWO_RANGE = (1.0, 20.0)
 PERI_SIDE_THREE_RANGE = (1.0, 20.0)
 
+int_to_peri_list = {
+    1   : 'single',
+    2   : 'pairs',
+    3   : 'triplets'
+}
+
 def make_single_prod(side_one_range):
     singles = list(np.linspace(side_one_range[0], side_one_range[1], num=100))
     
-    singles = [(x, None, None) for x in singles]
+    # singles = [(x, None, None) for x in singles]
     
     return singles
 
@@ -20,7 +26,7 @@ def make_pair_prod(side_one_range, side_two_range):
     
     # Generate all possible pairwise combinations
     pairs = list(product(side_one, side_two))
-    pairs = [(x, y, None) for x, y in pairs]
+    # pairs = [(x, y, None) for x, y in pairs]
     
     return pairs
 
