06ea27ee46b57cc6b36876c2605d4291b2baa47a
rishibathina <39679104+rishibathina@users.noreply.github.com>
2024-05-20 09:36:22 -0700

    added metadata for math and fixed some inheritence, need to do generate task now

--- taskverse/mathqa/geometry_task.py
+++ taskverse/mathqa/geometry_task.py
@@ -6,14 +6,19 @@ from tqdm import tqdm
 from ..base import TaskGenerator
 from ..task_store import TaskStore
 from utils import *
+import json
+import enum
+from metadata import MathTemplateMetaData
 # PERI_SIDE_ONE_RANGE, PERI_SIDE_TWO_RANGE, PERI_SIDE_THREE_RANGE, 
 #     make_single_prod, make_pair_prod, make_triplet_prod
 
-class Template(Enum):
+class Template(enum):
     PERIMETER_TEMPLATES = 'MathVerse/math_anotations/perimeter_templates.json'
 
 class GeoPlanGenerator(TaskGenerator):
-    def __init__(self, metadata={}, seed=42):
+    metadata: MathTemplateMetaData
+
+    def __init__(self, metadata: MathTemplateMetaData, seed=42):
         super().__init__(metadata, seed=seed)
     
     def _task_plan_to_str(self, task_plan) -> str:
@@ -37,20 +42,7 @@ class GeoPlanGenerator(TaskGenerator):
             
         }
 
-        # task = {
-        #     'question'      : question.replace('_', ' '),
-        #     'answer'        : answer.replace('_', ' '),
-        #     'options'       : [o.replace('_', ' ') for o in options],
-        #     'task_plan'     : self._task_plan_to_str(task_plan),
-        #     'image_metadata': image_metadata,
-        #     'image'         : self.make_image(image_metadata) if return_data else None
-        # }
-
         return task
-    
-    # def generate(self, task_plan, seed=None):
-    #     if seed == None:
-    #         self.rng = np.random.default_rng(seed=seed)
 
     
 class PerimeterGenerator(GeoPlanGenerator):
@@ -59,42 +51,22 @@ class PerimeterGenerator(GeoPlanGenerator):
         'side_one'          : 'float',
         'side_two'          : 'float',
         'side_three'        : 'float'
-        # 'solution'          : 'float',
     }
 
-    def __init__(self, seed=42):
-        super.__init__(seed=seed)
-        self.side_one_range = PERI_SIDE_ONE_RANGE
-        self.side_two_range = PERI_SIDE_TWO_RANGE
-        self.side_three_range = PERI_SIDE_THREE_RANGE
+    def __init__(self, metadata: MathTemplateMetaData, seed=42, side_one_range=PERI_SIDE_ONE_RANGE, side_two_range=PERI_SIDE_TWO_RANGE, side_three_range=PERI_SIDE_THREE_RANGE, num_splices=100):
+        super.__init__(metadata, seed=seed)
+        self.side_one_range = side_one_range
+        self.side_two_range = side_two_range
+        self.side_three_range = side_three_range
         self.int_to_peri_list = int_to_peri_list
-        
-    def handle_templates(template_path):
-        # iterate through json file and seperate into different number of params
-        templates_by_num_params = {}
-
-        with open(file_path, 'r') as file:
-            templates_data = json.load(file)
-
-        # Iterate over each template
-        for template_name, template_info in templates_data.items():
-            num_params = template_info["num_params"]
-            template_text = template_info["text"]
-
-            # Add the template text to the corresponding list based on the number of parameters
-            if num_params not in templates_by_num_params:
-                templates_by_num_params[num_params] = []
-            templates_by_num_params[num_params].append(template_text)
-
-        return templates_by_num_params
+        self.num_splices = num_splices
 
     def enumerate_task_plans(self, task_store: TaskStore):
-        single = make_single_prod(self.side_one_range)
-        pairs = make_pair_prod(self.side_one_range, self.side_three_range)
-        triplets = make_triplet_prod(self.side_one_range, self.side_two_range, self.side_three_range)
+        single = make_single_prod(self.side_one_range, self.num_splices)
+        pairs = make_pair_prod(self.side_one_range, self.side_three_range, self.num_splices)
+        triplets = make_triplet_prod(self.side_one_range, self.side_two_range, self.side_three_range, self.num_splices)
         
-        template_path = Template.PERIMETER_TEMPLATES
-        template_breakdown = handle_templates(template_path)
+        template_breakdown = self.metadata.templates_by_num_params
         
         for param_count, templates in template_breakdown.items():
             peri_list = locals()[self.int_to_peri_list[param_count]]
@@ -116,7 +88,6 @@ class PerimeterGenerator(GeoPlanGenerator):
                 
             
     def _generate_task(self, task_plan) -> Tuple[str | List[str] | Dict]:
-        
         pass
     
 # class AreaGenerator(GeoPlanGenerator):
--- taskverse/mathqa/utils.py
+++ taskverse/mathqa/utils.py
@@ -1,6 +1,7 @@
 import random
 
 import numpy as np
+from itertools import product
 # from PIL import Image
 
 PERI_SIDE_ONE_RANGE = (1.0, 20.0)
@@ -13,16 +14,16 @@ int_to_peri_list = {
     3   : 'triplets'
 }
 
-def make_single_prod(side_one_range):
-    singles = list(np.linspace(side_one_range[0], side_one_range[1], num=100))
+def make_single_prod(side_one_range, num_splices):
+    singles = list(np.linspace(side_one_range[0], side_one_range[1], num=num_splices))
     
     # singles = [(x, None, None) for x in singles]
     
     return singles
 
-def make_pair_prod(side_one_range, side_two_range):
-    side_one = np.linspace(side_one_range[0], side_one_range[1], num=100)
-    side_two = np.linspace(side_two_range[0], side_two_range[1], num=100)
+def make_pair_prod(side_one_range, side_two_range, num_splices):
+    side_one = np.linspace(side_one_range[0], side_one_range[1], num=num_splices)
+    side_two = np.linspace(side_two_range[0], side_two_range[1], num=num_splices)
     
     # Generate all possible pairwise combinations
     pairs = list(product(side_one, side_two))
@@ -31,10 +32,10 @@ def make_pair_prod(side_one_range, side_two_range):
     return pairs
 
 
-def make_triplet_prod(side_one_range, side_two_range. side_three_range):
-    side_one = np.linspace(side_one_range[0], side_one_range[1], num=100)
-    side_two = np.linspace(side_two_range[0], side_two_range[1], num=100)
-    side_three = np.linspace(side_three_range[0], side_three_range[1], num=100)
+def make_triplet_prod(side_one_range, side_two_range, side_three_range, num_splices):
+    side_one = np.linspace(side_one_range[0], side_one_range[1], num=num_splices)
+    side_two = np.linspace(side_two_range[0], side_two_range[1], num=num_splices)
+    side_three = np.linspace(side_three_range[0], side_three_range[1], num=num_splices)
     
     # Generate all possible triplets
     triplets = list(product(side_one, side_two, side_three))
